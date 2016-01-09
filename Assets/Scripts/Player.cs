89d19a5ede15a99c0abfc0b540e5d214b101109e
Taihei Ogitani <joetsu.saiko@gmail.com>
2016-01-09 19:00:35 +0900

    add sphire

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = new Vector3(Input.mousePosition.x,Input.mousePosition.y,10);
+						pos = Camera.main.ScreenToWorldPoint(pos);
+          	transform.position = pos;
+
+        }
+    }
+}

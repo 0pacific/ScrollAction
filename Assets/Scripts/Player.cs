8820aa175c3b8a36a5ff48c71fbcde8f832a2b38
Taihei Ogitani <joetsu.saiko@gmail.com>
2016-01-13 00:11:13 +0900

    enable to move sphire to pointed place

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    private const float FIRST_SPEED = 5;
+    bool moving = false;
+    Vector3 newPos;
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
-	// Update is called once per frame
-	void Update () {
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !moving)
         {
-            Vector3 pos = new Vector3(Input.mousePosition.x,Input.mousePosition.y,10);
-						pos = Camera.main.ScreenToWorldPoint(pos);
-          	transform.position = pos;
+            newPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10));
+            transform.position = Vector3.MoveTowards(transform.position, newPos, FIRST_SPEED * Time.deltaTime);
+            moving = true;
+        }
 
+        if (moving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, newPos, FIRST_SPEED * Time.deltaTime);
+            if (transform.position.Equals(newPos))
+            {
+                moving = false;
+            }
         }
     }
 }
